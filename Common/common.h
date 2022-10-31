e0a9f28ef0fa57b81e9f2daad622fe7cbaedd105
sqiportinhsa <terekhova-olga-5d@yandex.ru>
2022-10-31 11:56:23 +0300

    remove unused + fix codestyle

--- Common/common.h
+++ Common/common.h
@@ -6,19 +6,17 @@ const int Verification_const = 0xDEED;
 #define DEF_CMD(name, val, ...) CMD_##name = val,
 
 typedef enum {
-    MIN_CMD   = -1,
     #include "commands.h"
-    MAX_CMD   =  10,
     NOT_A_CMD =  11,
 } Commands;
 
 #undef DEF_CMD
 
 typedef enum {
-    CMD = 1 | (1 << 1) | (1 << 2) | (1 << 3),
-    VAL = 1 << 4,
-    REG = 1 << 5,
-    RAM = 1 << 6,
+    CMD  =  1 | (1 << 1) | (1 << 2) | (1 << 3),
+    VAL  =  1 << 4,
+    REG  =  1 << 5,
+    RAM  =  1 << 6,
 } Arg_bytes;
 
 #endif
