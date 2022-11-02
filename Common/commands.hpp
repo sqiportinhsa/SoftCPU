ee86da805979743cf70d8658d42584554b953aa8
sqiportinhsa <terekhova-olga-5d@yandex.ru>
2022-11-03 02:07:58 +0300

    fix jumps code

--- Common/commands.hpp
+++ Common/commands.hpp
@@ -54,7 +54,7 @@ DEF_CMD(POP, 7, POP__ARGS, {
 })
 
 DEF_CMD(JMP, 8, JUMP_ARGS, {
-    cpu->ip = *((size_t *) cpu->code + cpu->ip);
+    cpu->ip = *((int *) (cpu->code + cpu->ip) - 1);
 })
 
 #define DEF_JMP_IF(name, cmd_num, oper)                                \
@@ -63,7 +63,7 @@ DEF_CMD(JMP, 8, JUMP_ARGS, {
             second_popped = StackPop (cpu->cpu_stack, &cpu->stk_err);  \
                                                                        \
             if (first__popped oper second_popped) {                    \
-                cpu->ip = *((size_t *) (cpu->code + cpu->ip));         \
+                cpu->ip = *((int *) (cpu->code + cpu->ip) - 1);        \
             }                                                          \
         })
 
