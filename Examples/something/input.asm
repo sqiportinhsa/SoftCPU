927a42b85a7ae23772de69040de892037f42583b
sqiportinhsa <terekhova-olga-5d@yandex.ru>
2022-11-04 06:21:19 +0300

    one more example
    used for testing and debug

---
+++ Examples/something/input.asm
@@ -0,0 +1,38 @@
+push 10
+push 20
+ADD
+out
+call lol
+jmp br
+
+:lol
+push 1337
+out
+ret 
+
+:br
+push 228
+out
+in
+sqrt
+out
+jmp love_foxidokun
+hlt
+
+:love_foxidokun
+PUSH 10
+PUSH 20
+POP rax
+PUSH rax
+out
+push rax
+out
+jb 1
+ADD
+OUT
+:aBcd
+HLT
+:1
+PUSH 228
+OUT
+JMP abcd
