838584705187fdc9ab40c373e94fdea69e3acad1
sqiportinhsa <terekhova-olga-5d@yandex.ru>
2022-11-04 07:35:59 +0300

    add fibonacci

---
+++ Examples/fibonacci/fibonacci.asm
@@ -0,0 +1,33 @@
+;main
+in
+call fibonacci
+out
+out
+hlt
+
+:fibonacci
+push 1
+sub
+pop rax
+push rax
+push rax
+push 2
+ja n_eq_1_or_2
+call fibonacci
+push 1
+sub
+call fibonacci
+pop rax
+add
+push rax
+push 2
+add
+ret
+
+:n_eq_1_or_2
+pop rax
+push 1
+push rax
+push 1
+add
+ret
